63b8c3dcce0101496266560809198b9ec9455353
Jacob David <d.jacob30g@gmail.com>
2024-01-08 13:52:16 -0500

    created python_muscle sample bot

--- python_muscle/player.py
+++ python_muscle/player.py
@@ -44,6 +44,19 @@ class Player(Bot):
         my_cards = round_state.hands[active]  # your cards
         big_blind = bool(active)  # True if you are the big blind
         pass
+        
+        card1 = my_cards[0]
+        card2 = my_cards[1]
+
+        rank1 = card1[0] # "Ad", "9c", "Th" -> "A", "9", "T"
+        suit1 = card1[1] # "d", "c", "h", etc.
+        rank2 = card2[0]
+        suit2 = card2[1]
+
+        self.strong_hole = False
+        if rank1 == rank2 or (rank1 in "AKQJT" and rank2 in "AKQJT"):
+            self.strong_hole = True
+
 
     def handle_round_over(self, game_state, terminal_state, active):
         '''
@@ -97,10 +110,17 @@ class Player(Bot):
            max_cost = max_raise - my_pip  # the cost of a maximum bet/raise
            print(min_raise, max_raise, my_stack, opp_stack, my_pip, opp_pip)
         
+        if RaiseAction in legal_actions and len(my_cards) == 3:
+            return RaiseAction(max_raise)
+        if self.strong_hole == True and RaiseAction in legal_actions:
+            raise_amount = min_raise + (max_raise - min_raise) * 0.1
+            return RaiseAction(raise_amount)
         if CheckAction in legal_actions:
             return CheckAction()
         elif BidAction in legal_actions:
-            return BidAction(int(random.random()*my_stack)) # random bid between 0 and our stack
+            return BidAction(int(0.5*my_stack)) # random bid between 0 and our stack
+        elif self.strong_hole == False and FoldAction in legal_actions:
+            return FoldAction()
         return CallAction()
 
 
